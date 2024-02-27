0b40fc186ad79979e2b8f523ca3e9ef58e641133
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-27 18:42:26 +0100

    hw-11-func-separation

--- src/index.js
+++ src/index.js
@@ -2,29 +2,20 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 
 const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+
+function handleError(error) {
+  Notiflix.Notify.failure(`Wystąpił błąd: ${error}`);
+  errorElement.classList.remove('hidden');
+  loader.classList.add('hidden');
+}
 
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
   const queryString = event.target.searchQuery.value;
-  fetchImages(queryString);
-});
-
-function fetchImages(queryString) {
-  const fixedQueryString = queryString.split(' ').join('+');
-
-  axios
-    .get('https://pixabay.com/api/', {
-      params: {
-        key: '42529634-f4ee0a007b87bc585b0bc2cb3',
-        q: fixedQueryString,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      },
-    })
-    .then(response => response.data.hits)
-    .then(hits => {
-      const markupArray = hits.map(
+  fetchImages(queryString)
+    .then(({ data }) => {
+      const markupArray = data.hits.flatMap(
         ({
           webformatURL,
           largeImageURL,
@@ -46,8 +37,23 @@ function fetchImages(queryString) {
             </div>
           </div>`
       );
-
-      const gallery = document.querySelector('.gallery');
       gallery.innerHTML = markupArray.join(' ');
+    })
+    .catch(error => {
+      handleError(error);
     });
+});
+
+function fetchImages(queryString) {
+  const fixedQueryString = queryString.split(' ').join('+');
+
+  return axios.get('https://pixabay.com/api/', {
+    params: {
+      key: '42529634-f4ee0a007b87bc585b0bc2cb3',
+      q: fixedQueryString,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
 }
