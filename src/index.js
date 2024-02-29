48cc926abafcff535af9348ab7130cdb3ac068d7
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-29 14:53:31 +0100

    hw-11-pagination-done

--- src/index.js
+++ src/index.js
@@ -9,6 +9,17 @@ loadMoreBtn.classList.add('hidden');
 let currentPage = 1;
 let totalHits;
 
+function isFull() {
+  if (currentPage * 40 >= totalHits) {
+    Notiflix.Notify.failure(
+      `We're sorry, but you've reached the end of search results.`
+    );
+    loadMoreBtn.classList.add('hidden');
+  } else {
+    loadMoreBtn.classList.remove('hidden');
+  }
+}
+
 searchForm.addEventListener('submit', async event => {
   event.preventDefault();
   currentPage = 1;
@@ -19,10 +30,10 @@ searchForm.addEventListener('submit', async event => {
     const images = await fetchImages(fixedQueryString);
     showTotalHits(images);
     renderImages(images);
-    loadMoreBtn.classList.remove('hidden');
+    isFull();
   } catch (error) {
     Notiflix.Notify.failure(
-      `Wystąpił błąd przy pobieraniu/renderowaniu danych: ${error}`
+      `We have problems with loading these pictures: ${error}`
     );
   }
 });
@@ -33,17 +44,12 @@ loadMoreBtn.addEventListener('click', async () => {
   currentPage += 1;
 
   try {
-    if (currentPage * 40 > totalHits) {
-      Notiflix.Notify.failure(
-        `Nie znaleziono więcej pasujących obrazków spełniających wymagania`
-      );
-    } else {
-      const images = await fetchImages(fixedQueryString);
-      renderImages(images);
-    }
+    const images = await fetchImages(fixedQueryString);
+    renderImages(images);
+    isFull();
   } catch (error) {
     Notiflix.Notify.failure(
-      `Wystąpił błąd przy pobieraniu/renderowaniu danych: ${error}`
+      `We have problems with loading these pictures: ${error}`
     );
   }
 });
@@ -64,7 +70,7 @@ async function fetchImages(queryString) {
 
 function showTotalHits(data) {
   totalHits = data.data.totalHits;
-  Notiflix.Notify.success(`Znaleziono ${totalHits} pasujących odpowiedzi`);
+  Notiflix.Notify.success(`${totalHits} matching pictures were found`);
 }
 
 function renderImages(data) {
@@ -96,62 +102,3 @@ function renderImages(data) {
     gallery.innerHTML = markupArray.join(' ');
   }
 }
-
-//
-//STARY KOD
-// function handleError(error) {
-//   Notiflix.Notify.failure(`Wystąpił błąd: ${error}`);
-// }
-
-// searchForm.addEventListener('submit', event => {
-//   event.preventDefault();
-//   loadMoreBtn.classList.add('hidden');
-//   loadMoreBtn.classList.remove('hidden');
-//   currentPage = 1;
-//   const queryString = event.target.searchQuery.value;
-//   fetchImages(queryString)
-//     .then(({ data }) => {
-//       const markupArray = data.hits.flatMap(
-//         ({
-//           webformatURL,
-//           largeImageURL,
-//           tags,
-//           likes,
-//           views,
-//           comments,
-//           downloads,
-//         }) =>
-//           `<div class="photo-card">
-//             <a href="${largeImageURL}">
-//               <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//             </a>
-//             <div class="info">
-//               <p class="info-item"><b>Likes: ${likes}</b></p>
-//               <p class="info-item"><b>Views: ${views}</b></p>
-//               <p class="info-item"><b>Comments: ${comments}</b></p>
-//               <p class="info-item"><b>Downloads: ${downloads}</b></p>
-//             </div>
-//           </div>`
-//       );
-//       gallery.innerHTML = markupArray.join(' ');
-//     })
-//     .catch(error => {
-//       handleError(error);
-//     });
-// });
-
-// function fetchImages(queryString) {
-//   const fixedQueryString = queryString.split(' ').join('+');
-
-//   return axios.get('https://pixabay.com/api/', {
-//     params: {
-//       key: '42529634-f4ee0a007b87bc585b0bc2cb3',
-//       q: fixedQueryString,
-//       image_type: 'photo',
-//       orientation: 'horizontal',
-//       safesearch: true,
-//       per_page: 40,
-//       page: currentPage,
-//     },
-//   });
-// }
